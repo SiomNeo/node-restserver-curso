3281dbb6076bf68b745d3aea99198421419f0514
Sergio <sordonez19@gmail.com>
2020-06-06 14:03:44 -0600

    cambio en server.js

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.get('/usuario', function(req, res) {
-    res.json('get Usuario')
+    res.json('get Usuario local')
 })
 
 app.post('/usuario', function(req, res) {
